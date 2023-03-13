b4466c5ea249a186798e38df1d8d459f3f4a3f5d
Luis Vallejo <luisern29@gmail.com>
2023-03-13 12:32:12 -0500

    ajustando el id de usuarios

--- src/models/users.models.js
+++ src/models/users.models.js
@@ -1,69 +1,68 @@
-const db = require('../tools/database')
+const db = require("../tools/database");
 
-const { DataTypes } = require('sequelize')
+const { DataTypes } = require("sequelize");
 
-const Users = db.define('users', {
-    id:{
-        primaryKey: true, 
-        type: DataTypes.INTEGER,
-        allowNull: false
+const Users = db.define("users", {
+  id: {
+    primaryKey: true,
+    type: DataTypes.INTEGER,
+    autoIncrement: true,
+    allowNull: false,
+  },
+  firstName: {
+    type: DataTypes.STRING,
+    allowNull: false,
+    field: "first_name",
+  },
+  lastName: {
+    type: DataTypes.STRING,
+    allowNull: false,
+    field: "last_name",
+  },
+  email: {
+    type: DataTypes.STRING,
+    allowNull: false,
+    unique: true,
+    validate: {
+      isEmail: true,
     },
-    firstName:{
-        type: DataTypes.STRING,
-        allowNull: false,
-        field: 'first_name'
-    }, 
-    lastName:{
-        type: DataTypes.STRING,
-        allowNull: false,
-        field: 'last_name'
-    },
-    email:{
-        type: DataTypes.STRING,
-        allowNull: false,
-        unique: true,
-        validate: {
-            isEmail: true
-        }
-    },
-    password:{
-        type: DataTypes.STRING,
-        allowNull: false,
-        defaultValue: ''
-    },
-    phone:{
-        type: DataTypes.STRING,
-        allowNull: false,
-        defaultValue: ''
-    },
-    role:{
-        type: DataTypes.STRING,
-        allowNull: false,
-        defaultValue: 'normal'
-    },
-    area:{
-        type: DataTypes.STRING,
-        allowNull: false,
-        defaultValue: ''
-    },
-    job:{
-        type: DataTypes.STRING,
-        allowNull: false,
-        defaultValue: ''
-    },
-    status:{
-        type: DataTypes.STRING,
-        allowNull: false,
-        defaultValue: 'active'
-    },
-    isVerified:{
-        type: DataTypes.BOOLEAN,
-        allowNull: false,
-        field: 'is_verified',
-        defaultValue: false
-    }
-}, {
-    timestamps: false
-})
+  },
+  password: {
+    type: DataTypes.STRING,
+    allowNull: false,
+    defaultValue: "",
+  },
+  phone: {
+    type: DataTypes.STRING,
+    allowNull: false,
+    defaultValue: "",
+  },
+  role: {
+    type: DataTypes.STRING,
+    allowNull: false,
+    defaultValue: "normal",
+  },
+  area: {
+    type: DataTypes.STRING,
+    allowNull: false,
+    defaultValue: "",
+  },
+  job: {
+    type: DataTypes.STRING,
+    allowNull: false,
+    defaultValue: "",
+  },
+  status: {
+    type: DataTypes.STRING,
+    allowNull: false,
+    defaultValue: "active",
+  },
+  isVerified: {
+    type: DataTypes.BOOLEAN,
+    allowNull: false,
+    field: "is_verified",
+    defaultValue: false,
+  },
+});
 
-module.exports = Users
+module.exports = Users;
